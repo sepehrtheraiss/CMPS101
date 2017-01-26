2805ee851845ca769ad36c03cf17ff198a72dda5
sepehr <sepehr@eduroam-169-233-206-9.ucsc.edu>
2017-01-26 15:47:58 -0800

    completed list.c

--- PA/pa2/List.h
+++ PA/pa2/List.h
@@ -24,124 +24,123 @@ typedef struct ListObj* List;
 List newList(void);
 
 // freeList()
+// pre: *pL != NULL, pL != NULL
 // Frees all heap memory associated with List *pL, and sets *pL to NULL.
 void freeList(List* pL);
 
 // Access functions -----------------------------------------------------------
 
 // length()
-// pre:none
+// pre: L != NULL
 // Returns the length of L.
 int length(List L);
 
 //index()
-// pre:none
+// pre: L != NULL
 // If cursor is defined, returns the index of the cursor element,
 // otherwise returns -1.
 int index(List L);
 
 // front()
-// Pre: length()>0
+// Pre: L != NULL, length()>0
 // Returns front element.
 int front(List L);
 
 // back()
-// Pre: length()>0
+// Pre: L != NULL, length()>0
 // Returns back element. 
-int back();
+int back(List L);
 
 // get()
-// Pre: length()>0, index()>=0
+// Pre: L != NULL, length()>0, index()>=0
 // Returns cursor element.
-int get();
+int get(List L);
 
 // equals()
-// pre:none
+// pre: L != NULL
 // Returns true if this List and L are the same integer
 // sequence. The cursor is ignored in both lists.
-int equals(List L);
+int equals(List L,List C);
 
 // Manipulation procedures ----------------------------------------------------
 
 // clear()
-// pre:none
+// pre: L != NULL
 // Resets this List to its original empty state.
-void clear();
+void clear(List L);
 
 // moveFront()
-// pre:none
+// pre: L != NULL
 // If List is non-empty, places the cursor under the front element,
 // otherwise does nothing.
-void moveFront();
+void moveFront(List L);
 
 // moveBack()
-// pre:none
+// pre: L != NULL
 // If List is non-empty, places the cursor under the back element,
 // otherwise does nothing.
-void moveBack();
+void moveBack(List L);
 
 // movePrev()
-// pre: none
+// pre: L != NULL
 // If cursor is defined and not at front, moves cursor one step toward
 // front of this List, if cursor is defined and at front, cursor becomes
 // undefined, if cursor is undefined does nothing.
-void movePrev();
+void movePrev(List L);
 
 // moveNext()
-// pre: none
+// pre: L != NULL
 // If cursor is defined and not at back, moves cursor one step toward
 // back of this List, if cursor is defined and at back, cursor becomes
 // undefined, if cursor is undefined does nothing.
-void moveNext();
+void moveNext(List L);
 
 // prepend()
-// pre:none
+// pre: L != NULL
 // Insert new element into this List. If List is non-empty,
 // insertion takes place before front element.
-void prepend(int data);
+void prepend(List L,int data);
 
 // append()
-// pre:none
+// pre: L != NULL
 // Insert new element into this List. If List is non-empty,
 // insertion takes place after back element.
-void append(int data); 
+void append(List L,int data);
 
 // insertBefore()
-// Pre: length()>0, index()>=0
+// Pre: L != NULL,length()>0, index()>=0
 // Insert new element before cursor.
-void insertBefore(int data);
+void insertBefore(List L,int data);
 
 // insertAfter()
-// Pre: length()>0, index()>=0
+// Pre: L != NULL,length()>0, index()>=0
 // Inserts new element after cursor.
-void insertAfter(int data);
+void insertAfter(List L,int data);
 
 // deleteFront()
-// Pre: length()>0
+// Pre: L != NULL,length()>0
 // Deletes the front element. 
-void deleteFront();
+void deleteFront(List L);
 
 // deleteBack()
-// Pre: length()>0
+// Pre: L != NULL, length()>0
 // Deletes the back element.
-void deleteBack();
+void deleteBack(List L);
 
 // delete() 
-// Pre: length()>0, index()>=0
+// Pre: L != NULL, length()>0, index()>=0
 // Deletes cursor element, making cursor undefined.
-void delete();
+void delete(List L);
 
 // Other operations -----------------------------------------------------------
 
 // printList()
-// pre:none
-// Overrides Object's toString method. Returns a String
-// representation of this List consisting of a space
-// separated sequence of integers, with front on left.
+// pre: L != NULL
+// Prints data elements in L on a single line to stdout.
 void printList(FILE* out, List L);
 
 // copy()
-// pre:none
+// pre: L != NULL
 // Returns a new List representing the same integer sequence as this
 // List. The cursor in the new list is undefined, regardless of the
 // state of the cursor in this List. This List is unchanged.
